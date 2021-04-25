e02b5854b649359f198d5a4b573c0ccb2b8552aa
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 13:27:36 +0900

    Add list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="">
-    
-  </div>
+ <div class="page">
+   <ul>
+     <li v-for="post in posts" :key=post.id>
+        {{ post.title }}
+     </li>
+   </ul>
+ </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+ data () {
+   return {
+     posts: [],
+   }
+ },
+ mounted () {
+   axios.get('https://jsonplaceholder.typicode.com/posts')
+     .then((response) => this.posts = response.data)
+ }
+}
+</script>
