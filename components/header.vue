efa0e0aec505dfdbdef0770c36104d4062dbb437
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 13:16:39 +0900

    Add header

---
+++ components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="">Iteatter</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar{
+  background-color: #6d984c;
+}
+.navbar-brand{
+  color: white;
+  font-weight:600;
+}
+.navbar-brand:hover{
+  color: white;
+}
+</style>
