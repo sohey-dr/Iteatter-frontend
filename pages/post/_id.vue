62fa4cf8a1026a89b7998e5daf1dccfcf0bcace6
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 14:14:38 +0900

    Add post detail

---
+++ pages/post/_id.vue
@@ -0,0 +1,22 @@
+<template>
+ <div class="page">
+   {{ posts.Title }}
+   
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+ data () {
+   return {
+     posts: [],
+   }
+ },
+ mounted () {
+   axios.get('http://917f02cb43ed.ngrok.io/api/' + this.$route.params.id)
+     .then((response) => this.posts = response.data)
+ }
+}
+</script>
