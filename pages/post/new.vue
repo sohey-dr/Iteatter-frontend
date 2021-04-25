b287713e1bee7b5c43c4efe7a8be259425955147
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 14:43:17 +0900

    Add form

---
+++ pages/post/new.vue
@@ -0,0 +1,39 @@
+<template>
+ <div class="page mt-5 text-center">
+    <form @submit.prevent="post">
+      <p><input type="text" v-model="title" placeholder="title" name="title"/></p>
+      <p><input type="text" v-model="body" placeholder="body" name="body"/></p>
+      <div class="login-btn">
+        <button type="submit">提案</button>
+      </div>
+    </form>
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+ data () {
+   return {
+    valid: false,
+     title: [],
+     body: []
+   }
+ },
+ methods: {
+  post() {
+    axios.post('http://917f02cb43ed.ngrok.io/api/', {
+      Title: this.title,
+      Body: this.body
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+ }
+}
+</script>
