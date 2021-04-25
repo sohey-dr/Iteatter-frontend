2cb97aa3baeefcf11d86a58360c001c25a7f542a
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 13:16:33 +0900

    Add footer

---
+++ components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <p class="text-muted">mitei </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer{
+  position: absolute;/*←絶対位置*/
+  bottom: 0; /*下に固定*/
+  background-color: #3e3e3e;
+  width: 100%;
+}
+.container {
+  width: auto;
+  max-width: 680px;
+  padding: 0 15px;
+}
+.container {
+  margin: 20px 0;
+}
+</style>
